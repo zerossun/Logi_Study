d96aa35983c3317b35efd923bf6411b23fee446c
{wonsunyoung} <{wonsunyoung0709@gmail.com}>
2023-11-22 17:00:55 +0900

    modify

--- google/todo2.js
+++ google/todo2.js
@@ -33,6 +33,74 @@ function DeleteTodo(event) {
 //   console.log(li3.text);
 // }
 
+// PaintTodo : TODO를 그리는 역할
+function PaintTodo2(listitem) {
+  const li = document.createElement('li');
+  li.id = listitem.id;
+  li.text = listitem.text;
+  let span = document.createElement('span');
+  const Xbtn = document.createElement('button');
+  const Mbtn = document.createElement('button');
+  let Input = document.createElement('input');
+  Xbtn.addEventListener('click', DeleteTodo);
+
+  span.innerText = li.text;
+
+  Xbtn.innerText = '삭제';
+  Mbtn.innerText = '수정';
+
+  console.log(Input.value);
+  Mbtn.addEventListener('click', (event) => modify(event));
+
+  li.append(span, Xbtn, Mbtn, Input);
+  Input.classList.add(LOGIFORM_HIDDEN);
+
+  TodoList2.append(li);
+
+  const modify = (event, listitem) => {
+    if (Input.value == '') {
+      console.log('qwerty1');
+      Input.value = span.innerText;
+      span.classList.add(LOGIFORM_HIDDEN); // 뭐가 뭔지 모르지만 일단 숨겨짐.
+      Input.classList.remove(LOGIFORM_HIDDEN); // 뭐가 뭔지 모르지만 일단 나옴.
+    } else {
+      console.log('qwertytyuasdzfasdf');
+      span.classList.remove(LOGIFORM_HIDDEN);
+      Input.classList.add(LOGIFORM_HIDDEN);
+      SavedTodos1();
+    }
+
+    // 자 이제 저 input에 li.span 내용을 넣어주면 됨.
+    // 일단 input 안에 불러오긴 함.
+    // 자 이제 불러온 거를 수정하면 저장되게 만들면 됨. 문장 하나로 만들기는 쉽네
+    // 여러가지 방법을 썼지만 다시 턴백
+
+    // [도전했던 방법]
+    // 각각 이벤트 주면 같이 실행됨. -> 실패
+    // 함수 따로 빼서 주려함. -> 실력 미달로 함수에 지역변수 정보 옮기는 걸 못해서 실패
+    // 전역변수로 빼서 해봄 -> 기능 자체가 안됨. 그리고 솔직히 지역변수로 사용하고 싶음.
+    // 그래 계속 안되어봐라. 될 때까지 조진다.
+  };
+}
+
+// =============
+// const modify = (event, listitem) => {
+//   console.log('qwerty');
+//   const Mbtn = listitem.span;
+//   console.log(Mbtn);
+
+// 아씨 이렇게 하면 새로 span에 접근하는 게 아닌 새로 파짐;;;;;;
+// const todoItem = Mbtn.parentElement;
+// const todoItemTxt = todoItem.text;
+// Mbtn.innerText = todoItemTxt;
+
+//   const inputText = event.target.innerText;
+//   const inputMbtn = document.createElement('input');
+//   inputMbtn.value = inputText;
+//   inputMbtn.classList.add('edit-input');
+// };
+// ===============
+
 // function modify(listitem) {
 //   console.log('qwerty');
 //   const li3 = listitem.target.parentElement;
@@ -41,26 +109,6 @@ function DeleteTodo(event) {
 
 //   txt.classList.add(LOGIFORM_HIDDEN);
 // }
-
-// PaintTodo : TODO를 그리는 역할
-function PaintTodo2(listitem) {
-  const li = document.createElement('li');
-  li.id = listitem.id;
-  li.text = listitem.text;
-  const span = document.createElement('span');
-  const Xbtn = document.createElement('button');
-  const Mbtn = document.createElement('div');
-  Xbtn.addEventListener('click', DeleteTodo);
-  span.innerText = listitem.text;
-  Xbtn.innerText = '삭제';
-
-  Mbtn.classList.add('li');
-  Mbtn.addEventListener('click', (event) => modify(event, todo.id));
-  Mbtn.innerText = li.text;
-
-  li.append(span, Xbtn, Mbtn);
-  TodoList2.append(li);
-}
 
 function todolist(e) {
   e.preventDefault();
