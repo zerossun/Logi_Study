c42996b2d1e36ffddb947890771355d2f692e7b3
zerossun <wonsunyoung0703@gmail.com>
2023-11-24 03:49:37 +0900

    presentation

--- google/presentaiton.js
+++ google/presentaiton.js
@@ -2,7 +2,78 @@
 
 (function tset() {
   console.log(a);
-  let a = 'qwery';
+  var a = 'qwetry';
 })();
+// undefined
+
+// (function test(){
+//   console.log(b);
+//   let b = 'qwerty'
+// })();
+// ReferenceError: Cannot access 'a' before initialization
+
+// -> 둘이 값이 다르게 나오는 이유. 호이스팅!!!
+
 
 // closer
+
+// let l0 = 'l0';
+// function fn1(){
+//   let l1 = 'l1';
+//   console.log(l0,l1);
+// }
+// function fn2(){
+//   let l2 = 'l2';
+//   console.log(l0,l2);
+//   fn1();
+// }
+// fn2();
+// fn1();
+
+// let l0 = 'l0';
+// function fn1(){
+//   function fn2(){
+//     let l2 = 'l2';
+//     console.log(l0,l2);
+//     fn1();
+//   }
+//   let l1 = 'l1';
+//   fn2();
+// }
+// fn1();
+
+
+// =============================
+// 질문드렸던 코드
+var increase = function (num) {
+  return ++num;
+};
+
+var decrease = function (num) {
+  return --num;
+};
+
+var predicates = { increase, decrease };
+
+function makeCounter(predicate) {
+  var num = 0; // 0이라고 실행됨.
+
+  return function () {
+    num = predicate(num); // num은 predicate(num)과 같다고 정의됨.
+    return num;
+  };
+}
+
+var increaser = makeCounter(predicates.increase);
+console.log(increaser()); // 1
+console.log(increaser()); // 2
+
+var decreaser = makeCounter(predicates.decrease);
+console.log(decreaser()); // -1
+console.log(decreaser()); // -2
+
+
+// 함수의 유효범위는 어디서 실행됐느냐가 아닌 어디서 정의 됐느냐 - 생활코딩
+
+// ===========================
+
--- google/todo2.js
+++ google/todo2.js
@@ -18,7 +18,32 @@ function DeleteTodo(event) {
   SavedTodos1();
 }
 
+// [코딩은 기세다.]
+// 수정버튼을 클릭했을 때,
+// span 숨겨주고, input박스가 보여야 됨.
+// input 박스에는 li.span 정보가 써있어야 되고
+// 수정 버튼 다시 클릭했을 때, input박스 숨겨주고 li.span이 보여야 됨.
+// 삼항연산자로 했을 때,
+// 만약 li.span이 block으로 되어있으면, true일 때 숨김처리 & input 보여주기. : false 일 때, li.span block, input 숨김처리
+
+// 우선 그러면 버튼 클릭했을 때 li.text부터 가져오자. input이 나오는 기능 부터 만들어보자.
+// function modify(event) {
+//   console.log('qwerty');
+//   const li3 = event.target.parentElement;
+//   console.log(li3.text);
+// }
+
 // PaintTodo : TODO를 그리는 역할
+
+// 전역변수로 빼서 해봄 -> 기능 자체가 안됨. 그리고 솔직히 지역변수로 사용하고 싶음.
+// const li = document.createElement('li');
+// li.id = listitem.id;
+// li.text = listitem.text;
+// let span = document.createElement('span');
+// const Xbtn = document.createElement('button');
+// const Mbtn = document.createElement('button');
+// let Input = document.createElement('input');
+
 function PaintTodo2(listitem) {
   const li = document.createElement('li');
   li.id = listitem.id;
@@ -41,25 +66,58 @@ function PaintTodo2(listitem) {
   Input.classList.add(LOGIFORM_HIDDEN);
 
   TodoList2.append(li);
-
+// 현방법
   const modify = (event, listitem) => {
     if (Input.value == '') {
       console.log('qwerty1');
       Input.value = span.innerText;
-      span.classList.add(LOGIFORM_HIDDEN); // 뭐가 뭔지 모르지만 일단 숨겨짐.
-      Input.classList.remove(LOGIFORM_HIDDEN); // 뭐가 뭔지 모르지만 일단 나옴.
+      span.classList.add(LOGIFORM_HIDDEN);
+      Input.classList.remove(LOGIFORM_HIDDEN);
     } else {
       console.log('qwertytyuasdzfasdf');
       span.classList.remove(LOGIFORM_HIDDEN);
       Input.classList.add(LOGIFORM_HIDDEN);
       SavedTodos1();
-      span.innerText = Input.value; //화면에 나오는 거까지 성공함ㅜㅜㅜㅜㅜ
-      // 라스트, 이제 저장만 하면됨!!! 는 젤 난제네
+      span.innerText = Input.value;
     }
-
-    // 대리님코드를 보며 비교해보는 시간.
   };
+
+// =====================
+// [도전했던 방법]
+// 1. 각각 이벤트 주기 ->같이 실행됨. 실패
+//   const modify = (event, listitem) => {
+//     console.log('qwerty');
+    
+//     console.log(Mbtn); 
+  
+//   const todoItem = Mbtn.parentElement;
+//   const todoItemTxt = todoItem.text;
+//   Mbtn.innerText = todoItemTxt;
+  
+//     const inputText = event.target.innerText;
+//     const inputMbtn = document.createElement('input');
+//     inputMbtn.value = inputText;
+//     inputMbtn.classList.add('edit-input');
+//   };
 }
+
+    
+    
+    
+
+
+// =============
+
+// ===============
+// 2. 함수 따로 빼서 주려함. -> 실력 미달로 함수에 지역변수 정보 옮기는 걸 못해서 실패
+// function modify(listitem) {
+//   console.log('qwerty');
+//   const li3 = listitem.target.parentElement;
+//   const txt = li3.text;
+//   console.log(txt);
+
+//   txt.classList.add(LOGIFORM_HIDDEN);
+// }
 
 function todolist(e) {
   e.preventDefault();
@@ -89,3 +147,4 @@ if (savedTodos1 != null) {
 }
 
 TodoForm2.addEventListener('submit', todolist);
+
