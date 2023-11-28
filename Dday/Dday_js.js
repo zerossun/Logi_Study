2510b067f56bc722141cce732eb14d508ba1265d
{wonsunyoung} <{wonsunyoung0709@gmail.com}>
2023-11-28 18:30:39 +0900

    20231128

--- Dday/Dday_js.js
+++ Dday/Dday_js.js
@@ -1,24 +1,43 @@
-const Today = document.querySelector('#accent span');
-const now = new Date();
-const today = now.getDate();
-const first = new Date('2023-07-03');
-const firstT = first.getTime();
+// const Today = document.querySelector('#accent span');
+// const now = new Date();
+// const today = now.getDate();
+// const first = new Date('2023-07-03');
+// const firstT = first.getTime();
+// const toFirst = now - first;
+// const FirstDay = `${Math.round(toFirst / (60 * 60 * 1000 * 24))}일`;
+// Today.innerHTML = FirstDay;
 
-const toFirst = now - first;
-const FirstDay = `${Math.round(toFirst / (60 * 60 * 1000 * 24))}일`;
-Today.innerHTML = FirstDay;
+// function calc(e) {
+//   const date = firstT + e / (60 * 60 * 1000 * 24);
+//   const dateD = new Date(date);
+//   const year = dateD.getFullYear();
+//   const month = dateD.getMonth();
+//   const day = dateD.getDay();
+//   document.querySelector('#date' + e).innerText =
+//     year + '년' + month + '월' + day + '일';
+// }
+// calc(100);
+// calc(200);
+// calc(365);
+// calc(500);
 
-
-
-function calc(e){
-    const date = firstT + e / (60 * 60 * 1000 * 24);
-    const dateD = new Date(date);
-    const year = dateD.getFullYear();
-    const month = dateD.getMonth();
-    const day = dateD.getDay();
-    document.querySelector('#date' + e).innerText = year +'년' + month + '월' + day + '일'; 
-};
-calc(100);
-calc(200);
-calc(365);
-calc(500);
+function calcModified(dayAdded) {
+  // geTime 이 밀리초를 반환하기 때문에
+  // 일 수(day)를 밀리초 단위로 환산하기 위해 24(하루의 시간) * 60(한 시간의 분) * 60(일 분의 초) * 1000(초 => 밀리 초) 를 day에 곱해 줌
+  const addedMilliseconds =
+    todayMilliseconds + dayAdded * (24 * 60 * 60 * 1000);
+  // 계산된 밀리초를 가지고 날짜 객체 새로 생성
+  const dateAfter = new Date(addedMilliseconds);
+  // 연도
+  const year = dateAfter.getFullYear();
+  // 월 0~11 반환이므로 1을 더해야 실제 달과 같아짐
+  const month = dateAfter.getMonth() + 1;
+  // getDay()는 요일을 일요일~토요일(0~6) 로 반환하기 때문에 getDay 가 아닌 getDate 호출
+  const day = dateAfter.getDate();
+  return `${year}-${month}-${day}`;
+}
+console.log('=============수정본 출력결과=============');
+console.log(calcModified(100));
+console.log(calcModified(200));
+console.log(calcModified(365));
+console.log(calcModified(500));
--- google/login2.js
+++ google/login2.js
@@ -5,6 +5,12 @@ const Singin = document.querySelector('#LoginForm button');
 const greeting = document.querySelector('#greeting');
 const Logout = document.querySelector('#logout');
 
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+
 const HIDDEN = 'hidden';
 
 function LoginBtn2(e) {
@@ -50,3 +56,96 @@ if (setItem2 === null) {
 LoginForm.addEventListener('submit', LoginBtn2);
 Logout.addEventListener('click', LogOutBtn2);
 LoginInput.addEventListener('change', LoginInputChnage);
+
+// // form
+
+// // Show input error message
+// const error = (input, message) => {
+//   const formInput = input.parentElement;
+//   formInput.className = 'form-control error';
+//   const small = formInput.querySelector('small');
+//   small.innerText = message;
+// };
+
+// // Show success outline
+// const success = (input) => {
+//   const formInput = input.parentElement;
+//   formInput.className = 'form-control success';
+// };
+
+// //Check email is valid
+// const isValidEmail = (email) => {
+//   const re =
+//     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   if (re.test(email.value)) {
+//     success(email);
+//   } else {
+//     error(email, 'Email is not valid');
+//   }
+// };
+
+// // Q. Get field name = 얜 뭐하는 기능이지..?
+// //    input.id.charAt(0).toUpperCase() : input.id의 첫번째를 대문자로 바꿔주거라
+// //    input.id.slice(1) : input.id의 첫번째부터 잘라서 보여줘라 이것도 알겠음.
+// //    고로 첫글자를 대문자로 바꿔서 둘이 붙이라는 거 같은데, 이 기능을 왜 넣어야 하는거지?
+// // Q. input.id는 변수의 id를 뜻하는거임.
+// //    고로, id="password2"이거니 첫번째 p는 P로 나머지는 그냥 붙여주겠다라는 코드!!
+// const getFieldName = (input) => {
+//   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+// };
+
+// // Check required fields
+// const checkRequired = (inputArr) => {
+//   inputArr.forEach((input) => {
+//     if (input.value.trim() === '') {
+//       error(input, `${getFieldName(input)} is requrired `);
+//     } else {
+//       success(input);
+//     }
+//   });
+// };
+
+// // Check input length
+// const checkLength = (input, min, max) => {
+//   if (input.value.length < min) {
+//     error(input, `${getFieldName(input)} must be at least ${min} characters`);
+//   } else if (input.value.length > max) {
+//     error(
+//       input,
+//       `${getFieldName(input)} must be at less than ${max} characters`
+//     );
+//   }
+// };
+// //check passwords match
+// const checkPasswordsMatch = (input1, input2) => {
+//   if (input1.value !== input2.value) {
+//     error(input2, 'Password do not match');
+//   }
+// };
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   checkRequired([username, email, password, password2]);
+//   checkLength(username, 8, 15);
+//   checkLength(password, 6, 25);
+//   isValidEmail(email);
+//   checkPasswordsMatch(password, password2);
+// });
+// // form
+
+// 유효성 검사 연습1
+// 1. 오류가 났을 때 어떻게 나와야 하는가.
+// 1-1. 오류가 났을 때 하단에 어떤 문구가 나와야 하는가
+// 1-2. 각각 오류문구에 해당 이름을 어떻게 넣어야 하는가
+// 2. 성공했을 때 어떻게 나와야 하는가.
+// 3-1. username 유효성은 무엇인가.
+// 3-2. email의 유효성은 무엇인가.
+// 3-3. password의 유효성은 무엇인가.
+// 3-4. password의 유효성은 무엇인가.
+// 3-5. password2의 유효성은 무엇인가.
+// 4. 어디에 기능을 넣을것인가.
+
+const error = (input) => {
+  const qwerty = input.parentElement;
+};
