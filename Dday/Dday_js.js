eef68e3e7cebbe4551904e30b338875db78fb526
{wonsunyoung} <{wonsunyoung0709@gmail.com}>
2023-12-01 18:29:11 +0900

    20231201

--- Dday/Dday_js.js
+++ Dday/Dday_js.js
@@ -13,52 +13,52 @@ const day2 = dayjs().year();
 //getTime
 console.log(day2);
 
-// // getFullYear
-// console.log(day1.getFullYear());
-// // getMonth
-// const month = day1.getMonth() + 1;
-// console.log(month);
-// // getDate
-// console.log(day1.getDate());
+// getFullYear
+console.log(day1.getFullYear());
+// getMonth
+const month = day1.getMonth() + 1;
+console.log(month);
+// getDate
+console.log(day1.getDate());
 
-// //// Dday - 순수 자바스크립트
-// const Today = document.querySelector('#accent span');
-// // 만나 기념일 일수 넣을 값 변수 지정
-// const now = new Date();
-// // 현재 날짜 지정
-// const today = now.getDate();
-// // 현재 날짜를 밀리초수로 반환한 변수
-// const first = new Date('2023-07-03');
-// // 만난 날짜 지정한 변수
-// const firstT = first.getTime();
-// // 만난 날짜 밀리초수로 반환한 변수
-// const toFirst = now - first;
-// // 현재 날짜 초수 - 만난 날자 초수
-// const FirstDay = `${Math.round(toFirst / (60 * 60 * 1000 * 24))}일`;
-// // toFirst초수를 일수로 변경
-// Today.innerHTML = FirstDay;
-// // 화면에 보이게 함
+//// Dday - 순수 자바스크립트
+const Today = document.querySelector('#accent span');
+// 만나 기념일 일수 넣을 값 변수 지정
+const now = new Date();
+// 현재 날짜 지정
+const today = now.getDate();
+// 현재 날짜를 밀리초수로 반환한 변수
+const first = new Date('2023-07-03');
+// 만난 날짜 지정한 변수
+const firstT = first.getTime();
+// 만난 날짜 밀리초수로 반환한 변수
+const toFirst = now - first;
+// 현재 날짜 초수 - 만난 날자 초수
+const FirstDay = `${Math.round(toFirst / (60 * 60 * 1000 * 24))}일`;
+// toFirst초수를 일수로 변경
+Today.innerHTML = FirstDay;
+// 화면에 보이게 함
 
-// function calc(e) {
-//   // 중복되는 코드이므로 함수로 지정하여 매개변수로 넣어줌
-//   const date = firstT + e * (60 * 60 * 1000 * 24);
-//   // 현재 날짜 밀리초 + 지정한 날짜이후 밀리초
-//   // 지정한 날짜이후 밀리초 : 지정일 * 24(하루의 시간) * 60(한 시간의 분) * 60(일 분의 초) * 1000(초 => 밀리 초)
+function calc(e) {
+  // 중복되는 코드이므로 함수로 지정하여 매개변수로 넣어줌
+  const date = firstT + e * (60 * 60 * 1000 * 24);
+  // 현재 날짜 밀리초 + 지정한 날짜이후 밀리초
+  // 지정한 날짜이후 밀리초 : 지정일 * 24(하루의 시간) * 60(한 시간의 분) * 60(일 분의 초) * 1000(초 => 밀리 초)
 
-//   // 매개변수의 값
-//   const dateD = new Date(date);
-//   // 더한 밀리초를 날짜로 바꿔줌
-//   const year = dateD.getFullYear();
-//   // 바꾼 날짜에서 연도값 빼내는 변수
-//   const month = dateD.getMonth() + 1;
-//   // 바꾼 날짜에서 월값 빼내는 변수
-//   const day = dateD.getDate();
-//   // getDay()는 요일을 일요일~토요일(0~6) 로 반환하기 때문에 getDay 가 아닌 getDate 호출
-//   // getDate() : 날짜 출력, getDay() : 요일 출력(0~6)
-//   document.querySelector('#date' + e).innerText =
-//     year + '년' + month + '월' + day + '일';
-//   // 리턴된 숫자들 html text에 넣어주기
-// }
+  // 매개변수의 값
+  const dateD = new Date(date);
+  // 더한 밀리초를 날짜로 바꿔줌
+  const year = dateD.getFullYear();
+  // 바꾼 날짜에서 연도값 빼내는 변수
+  const month = dateD.getMonth() + 1;
+  // 바꾼 날짜에서 월값 빼내는 변수
+  const day = dateD.getDate();
+  // getDay()는 요일을 일요일~토요일(0~6) 로 반환하기 때문에 getDay 가 아닌 getDate 호출
+  // getDate() : 날짜 출력, getDay() : 요일 출력(0~6)
+  document.querySelector('#date' + e).innerText =
+    year + '년' + month + '월' + day + '일';
+  // 리턴된 숫자들 html text에 넣어주기
+}
 
 // calc(100);
 // calc(200);
--- Dday/Dday_js2.js
+++ Dday/Dday_js2.js
@@ -1,26 +1,26 @@
 function calcModified(dayAdded) {
-    const first = new Date('2023-07-03');
-    const todayMilliseconds = first.getTime();
-    // geTime 이 밀리초를 반환하기 때문에
-    // 일 수(day)를 밀리초 단위로 환산하기 위해 24(하루의 시간) * 60(한 시간의 분) * 60(일 분의 초) * 1000(초 => 밀리 초) 를 day에 곱해 줌
-    // 현재 날짜 밀리초 + 지정한 날짜이후 밀리초
-    const addedMilliseconds =
-      todayMilliseconds + dayAdded * (24 * 60 * 60 * 1000);
-    // 계산된 밀리초를 가지고 날짜 객체 새로 생성
-    const dateAfter = new Date(addedMilliseconds);
-    // 더한 밀리초를 날짜로 바꿔줌
-    console.log(dateAfter); // 결과값 : Fri Jan 19 2024 09:00:00 GMT+0900 (한국 표준시)
-    // 바꾼 날짜에서 연도값 빼내는 변수
-    const year = dateAfter.getFullYear();    
-    // 바꾼 날짜에서 월값 빼내는 변수
-    // 월 0~11 반환이므로 1을 더해야 실제 달과 같아짐
-    const month = dateAfter.getMonth() + 1;
-    // getDay()는 요일을 일요일~토요일(0~6) 로 반환하기 때문에 getDay 가 아닌 getDate 호출
-    // getDate() : 날짜 출력, getDay() : 요일 출력(0~6)
-    const day = dateAfter.getDate();
-    return `${year}-${month}-${day}`;
-  }
-  document.querySelector('#date100').innerText = calcModified(100);
-  document.querySelector('#date200').innerText = calcModified(200);
-  document.querySelector('#date365').innerText = calcModified(365);
-  document.querySelector('#date500').innerText = calcModified(500);
+  const first = new Date('2023-07-03');
+  const todayMilliseconds = first.getTime();
+  // geTime 이 밀리초를 반환하기 때문에
+  // 일 수(day)를 밀리초 단위로 환산하기 위해 24(하루의 시간) * 60(한 시간의 분) * 60(일 분의 초) * 1000(초 => 밀리 초) 를 day에 곱해 줌
+  // 현재 날짜 밀리초 + 지정한 날짜이후 밀리초
+  const addedMilliseconds =
+    todayMilliseconds + dayAdded * (24 * 60 * 60 * 1000);
+  // 계산된 밀리초를 가지고 날짜 객체 새로 생성
+  const dateAfter = new Date(addedMilliseconds);
+  // 더한 밀리초를 날짜로 바꿔줌
+  console.log(dateAfter); // 결과값 : Fri Jan 19 2024 09:00:00 GMT+0900 (한국 표준시)
+  // 바꾼 날짜에서 연도값 빼내는 변수
+  const year = dateAfter.getFullYear();
+  // 바꾼 날짜에서 월값 빼내는 변수
+  // 월 0~11 반환이므로 1을 더해야 실제 달과 같아짐
+  const month = dateAfter.getMonth() + 1;
+  // getDay()는 요일을 일요일~토요일(0~6) 로 반환하기 때문에 getDay 가 아닌 getDate 호출
+  // getDate() : 날짜 출력, getDay() : 요일 출력(0~6)
+  const day = dateAfter.getDate();
+  return `${year}-${month}-${day}`;
+}
+document.querySelector('#date100').innerText = calcModified(100);
+document.querySelector('#date200').innerText = calcModified(200);
+document.querySelector('#date365').innerText = calcModified(365);
+document.querySelector('#date500').innerText = calcModified(500);
