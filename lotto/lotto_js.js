dff53c4090f86bd8dcfc124cb7a144ea6d2b535c
zerossun <wonsunyoung0703@gmail.com>
2023-12-05 01:58:07 +0900

    map 변수 꺼내기 실패 과정

--- lotto/lotto_js.js
+++ lotto/lotto_js.js
@@ -2,17 +2,20 @@ const result = document.querySelector('#result');
 const result1 = document.querySelector('#result1');
 const lottoBtn = document.querySelector('#lottoBtn');
 
+
+const RAN = 'ran';
 // random 함수 해석:
 // candiate라는 변수에 45개를를 배열해줘라.
 // fill(); 그리구 꽉 채워줘.(이러면 undefined로 꽉채워줌.)
 // map() : index에 1씩 더해서 다시 값 뽑아줘라
-function random(e) {
+function random() {
   let candiate = Array(45)
-    .fill()
-    .map((item, index) => {
-      return item, index + 1;
-    });
-
+  .fill()
+  .map((item, index) => {
+    return item, index + 1;
+  });
+  
+  
   // console.log(candiate);
 
   const random = candiate.sort(() => Math.random() - 0.5);
@@ -29,17 +32,20 @@ function random(e) {
   // sort는 원본배열을 변경하니 random변수 내리면 변경된 변수의 마지막 값을 빼오는 거고
   // 다만, 저 화면에 띄워주는 slice 변수에 slice배열을 넣어서 맨 뒤에가 안 보여져 내가 혼란이 온 거였음!
   // 고민 해결!!
+const span = document.createElement('span');
 
   let slice = random.slice(0, 4).sort((a, b) => {
     return a - b;
   });
-
-  let sliceT = slice.map((slice1, index) => {
-    console.log(slice1, index);
+span.innerText = slice;
+  let sliceT = slice.map((slice1) => {
+    console.log(slice1);
+    // slice1.classList.add('ran');
     return slice1;
   });
   // console.log(slice1);
-  console.log(sliceT);
+ 
+  // sliceT.classList.add('ran');
   // slice 변수 해석:
   // 랜덤배열을 시작부터 4변째 자리 자르는데 sort를 이용해서 순서대로 나열해줘라.
   console.log(slice);
@@ -50,8 +56,12 @@ function random(e) {
 
   // console.log(slice[0]);
   console.log('number', slice, 'bonus', bonus);
-  result.innerText = sliceT;
+  console.log(slice[0]);
+  // result.innerText = slice;
+  result.append(span);
+  span.classList.add(RAN);
   result1.innerText = bonus;
+  // ballCSS();
 }
 function hidden(e) {
   result.classList.remove('hidden');
@@ -60,29 +70,4 @@ function hidden(e) {
 
 lottoBtn.addEventListener('click', hidden);
 
-function ballCSS(number) {
-  let ball = result;
-  ball.textContent = number;
-  ball.style.display = 'inline-block';
-  ball.style.border = '1px solid black';
-  ball.style.borderRadius = '100%'; // JS에서는 -가 '빼기'이므로 camel표기법으로 대체
-  ball.style.width = '20px';
-  ball.style.height = '20px';
-  ball.style.textAlign = 'center';
-  ball.style.padding = '10px';
-  ball.style.margin = '10px';
-  let backGroundColor; // 여기부터
-  if (number <= 10) {
-    backGroundColor = 'red';
-  } else if (number <= 20) {
-    backGroundColor = 'orange';
-  } else if (number <= 30) {
-    backGroundColor = 'yellow';
-  } else if (number <= 40) {
-    backGroundColor = 'blue';
-  } else {
-    backGroundColor = 'green';
-  }
-  ball.style.background = backGroundColor;
-  ball.style.fontWeight = 'bold'; // 여기까지 추가함.
-}
+
