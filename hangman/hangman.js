d01ecae5757c584786086cbe66cb6ffdfae2a8b5
zerossun <wonsunyoung0703@gmail.com>
2023-12-15 01:58:49 +0900

    20231215_1

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,4 +1,4 @@
-// html 영역 지정
+
 const first = document.querySelector('#first');
 const content = document.querySelector('#content');
 const end = document.querySelector('#end');
@@ -12,7 +12,7 @@ const next = document.querySelector('.next');
 const score = document.querySelector('.score');
 const replay = document.querySelector('.replay');
 
-// 이미지 배열
+
 const images = [
   './img/0.png',
   './img/1.png',
@@ -23,10 +23,10 @@ const images = [
   './img/6.png',
   './img/7.png',
 ];
-// css 숨김 기능
+
 const HIDDEN = 'hidden';
 
-// 문제 배열
+
 const quiz_Q = [
   {
     quizImg: images[0],
@@ -78,9 +78,6 @@ const quiz_Q = [
   },
 ];
 
-// 익명함수 내에 지역변수로 각 html 코드 제작.
-// 위에서 선언한 변수에 html 집어넣기
-// 파라미터를 index로 지정 후 하단의 next 버튼 기능에서 함수에 따라 파라미터 수가 바뀔 수 있도록 제작
 const quiz_List = (index) => {
   let quizI = `
   <em>${index + 1}</em>
@@ -122,69 +119,51 @@ const quiz_List = (index) => {
   quiz_img.innerHTML = quizQ;
   quiz__desc.innerHTML = quizD;
 
-  //위의 label spand의 모든 요소를 변수로 지정
+  
   const quizChoiceSpan = content.querySelectorAll('.quiz_choice label span');
-  // quizChoiceSpan수만큼 변수내애 choiceSelectedr라는 클릭 이벤트 리스너를 넣어줘라.
+  
   for (let i = 0; i < quizChoiceSpan.length; i++) {
     quizChoiceSpan[i].setAttribute('onclick', 'choiceSelected(this)');
   }
-  // css적 요소
+  
   next.classList.add(HIDDEN);
   quiz__desc.classList.add(HIDDEN);
 };
 
-// span 클릭 시 일어나는 기능.
 function choiceSelected(answer) {
-  // 매개변수의 innerText 변수로 지정 / 클릭한 span의 텍스트
   let userAnsewr = answer.innerText;
-  // 배열의 인덱스에 맞는[현재 카운트 수] quizCho(문제답)을 변수로 지정
   let currentAnswer = quiz_Q[quizCount].quizCho;
-  // 만약 클릭한 span의 텍스트와 현재 카운트의 문제 답이 같다면,
-  // = 로 할 시, = : 할당연산자 같다라는 평가 아닌 값이 대입됨. 애초에 안됨.
-  // == 로 할 시, == : 값만 같으면 true라고는 나옴. 하지만, 데이터 종류가 달라도 같게 나옴
-  // === 로 할 시, === : 엄격하게 관리, 데이터 종류도 같은지 확인해줌.
   if (userAnsewr === currentAnswer) {
-    // score 점수를 1점 높여줘라
+   
     quizScore = quizScore + 1;
   }
-  // css적 요소
   next.classList.remove(HIDDEN);
   quiz__desc.classList.remove(HIDDEN);
 }
 
-// '다음 문제' 버튼 클릭 시 이벤트 리스너 기능
 next.addEventListener('click', () => {
-  // 정확히 지정하기에 화살표 함수 사용
-  // 만약 현 카운터 수가 문제 배열의 수 -1 이랑 같다면 하단의 css를 조정해줘라.
   if (quizCount == quiz_Q.length - 1) {
     next.classList.add(HIDDEN);
     content.classList.add(HIDDEN);
     end.classList.remove(HIDDEN);
-    // 아니마련 1을 더해줘라.
   } else {
     quizCount++;
   }
-  // 함수 호출
   quiz_List(quizCount);
-  // socre안의 텍스트 지정
   score.innerText = `당신의 점수는 ${quizScore} 입니다`;
 });
 
-// '시작하기' 버튼 클릭 시 이벤트 리스너 기능
 function startBtn(e) {
   e.preventDefault();
   first.classList.add(HIDDEN);
   content.classList.remove(HIDDEN);
-  // 문제 수, 점수 0으로 지정
   quizScore = 0;
   quizCount = 0;
-  // 함수 호출
   quiz_List(quizCount, quizScore);
 }
 
 start.addEventListener('click', startBtn);
 
-// '시작하기' 버튼 클릭 시 이벤트 리스너 기능. css 조정
 replay.addEventListener('click', () => {
   content.classList.add(HIDDEN);
   end.classList.add(HIDDEN);
