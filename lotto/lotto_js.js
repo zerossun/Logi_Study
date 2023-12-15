3aa573fa3cb36748c34423844d07c4315654a72b
김 동규 <tkillman@logiall.com>
2023-12-15 11:51:46 +0900

    로또 로직 수정

--- lotto/lotto_js.js
+++ lotto/lotto_js.js
@@ -5,8 +5,20 @@ const bonusBtn = document.querySelector('#bonusBtn');
 
 const RAN = 'ran';
 
+const defaultChoicedNums = [];
+
+let choicedNums = defaultChoicedNums;
+
+const setChoicedNums = (paramChoicedNums) => {
+  choicedNums = paramChoicedNums;
+};
+
+const sortRandomArray = (paramArray) => {
+  return paramArray.sort(() => Math.random() - 0.5);
+};
+
 const candiates1 = () =>
-  Array(45)
+  Array(8)
     .fill()
     .map((item, index) => {
       return item, index + 1;
@@ -23,11 +35,18 @@ const set = (value, index) => {
 
 function random() {
   const candiate = candiates1();
-  const random = candiate.sort(() => Math.random() - 0.5);
-  let slice = random.slice(0, 4).sort((a, b) => {
-    return a - b;
-  });
-  
+  const random = sortRandomArray(candiate);
+  console.log('random', random);
+
+  // ?? 로또번호는 6개 아닌가, 랜덤 6개 뽑은걸 왜 또 sort 하는걸까?
+  // let slice = random.slice(0, 6).sort((a, b) => {
+  //   return a - b;
+  // });
+
+  let slice = random.slice(0, 6);
+  // 선택된 번호들을 기억
+  setChoicedNums(slice);
+
   for (let i = 0; i < slice.length; i++) {
     set(slice[i], i);
   }
@@ -38,8 +57,17 @@ function hidden() {
   random();
 }
 function bonusB() {
+  if (choicedNums.length === 0) {
+    window.alert('아직 번호들을 뽑지 않으셨습니다.');
+    return;
+  }
+
   const candiate = candiates1();
-  const bonus = candiate[candiate.length - 1];
+  const remainCandiate = candiate.filter((r) => !choicedNums.includes(r));
+  console.log('remainCandiate', remainCandiate);
+  // TODO slice와 splice 정리하기
+  const bonus = sortRandomArray(remainCandiate).slice(0, 1);
+  console.log('bonus', bonus);
   result1.classList.add(RAN);
   result1.innerText = bonus;
 }
